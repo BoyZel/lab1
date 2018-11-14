7f70be7971cbf3594508d7c7d7d79c8fce9495ea
BoyZel <mateuszrasmus@protonmail.ch>
2018-11-14 14:46:26 +0100

    changed interface, added 1st test

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-list: main.o product.o fridge.o list.o
-	g++ main.o product.o fridge.o list.o -o list
+list: main.o product.o fridge.o list.o test.o interface.o
+	g++ main.o product.o fridge.o list.o test.o interface.o -o list
 	rm *o
 main.o: main.cpp
 	g++ -c main.cpp
@@ -9,3 +9,7 @@ fridge.o: fridge.cpp
 	g++ -c fridge.cpp
 list.o: list.cpp
 	g++ -c list.cpp
+test.o: test.cpp
+	g++ -c test.cpp
+interface.o: interface.cpp
+	g++ -c interface.cpp
--- main.cpp
+++ main.cpp
@@ -1,34 +1,11 @@
 #include "product.h"
 #include "fridge.h"
 #include "list.h"
+#include "test.h"
+#include "interface.h"
 using namespace std;
 int main(){
-	Product onion("onion",0,2.99);
-	Product yoghurt("yoghurt",0,1.99);
-	Product beer("beer",0,3.49);
-	Product vodka("vodka",0,19.99);
-	Status fridge;
-	fridge.add(onion);
-	fridge.add(yoghurt);
-	fridge.add(beer);
-	fridge.add(vodka);
-	fridge.change_status("onion",5);
-	cout<<"FRIDGE"<<endl;
-	fridge.show_all();
-	Status prefer;
-	prefer.add(onion);
-	prefer.add(yoghurt);
-	prefer.add(beer);
-	prefer.add(vodka);
-	prefer.change_status("yoghurt",4);
-	prefer.change_status("beer",2);
-	prefer.change_status("vodka",11);
-	cout<<endl<<"PREFERENCES"<<endl;
-	prefer.show_all();
-	cout<<endl<<"LIST"<<endl;
-	List shopping;
-	shopping.make(prefer,fridge);
-	shopping.show_all();
-
+	Interface menu;
+	menu.interface();
 	return 0;
 }
